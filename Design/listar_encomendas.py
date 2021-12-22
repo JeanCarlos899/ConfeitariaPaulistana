396000953d3a029c4c5660b7bc594a8d96584ae9
Jean Carlos <jeancarllos99@outlook.com>
2021-12-22 13:07:51 -0300

    att

--- Design/listar_encomendas.py
+++ Design/listar_encomendas.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg 
-# from Scripts.print_table import print_table
+from Scripts.print_table import print_table
  
 class ListarEncomendas:
     def menu_encomendas():
@@ -15,7 +15,7 @@ class ListarEncomendas:
         sg.theme('Dark Blue 3')
         layout = [
             [sg.Text("Lista de encomendas", font=("Helvetica", 15))],
-            # [sg.Text(print_table(), font=("Courier New", 12))], #Fonte monoespaçada para ficar alinhado
+            [sg.Text(print_table(), font=("Courier New", 12))], #Fonte monoespaçada para ficar alinhado
             [sg.Button("Voltar", size=(100,2))],
         ]
         return sg.Window("Listar encomendas", layout=layout, finalize=True)
