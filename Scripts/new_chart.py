24b1738ec787277b5bb01a12be79f110b6f52e81
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-12-23 13:49:05 -0300

    Att

--- Scripts/new_chart.py
+++ Scripts/new_chart.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
-arquivo = pd.read_excel('Dados_Cobaia.xlsx')
+arquivo = pd.read_excel('dados.xlsx')
 
 
 
 def graficoPizza():
-    Entregue = arquivo.loc[arquivo['Status']=='Entregue']
+    Entregue = arquivo.loc[arquivo['Status']=='Concluído']
     Pendente = arquivo.loc[arquivo['Status']=='Pendente']
     plt.title('Status dos pedidos')
-    plt.pie([len(Entregue),len(Pendente)], labels=['Entregue','Pendente'], autopct='%1.1f%%')
+    plt.pie([len(Entregue),len(Pendente)], labels=['Concluído','Pendente'], autopct='%1.1f%%')
     plt.axis('equal')
     plt.show()
 
