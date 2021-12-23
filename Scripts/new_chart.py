e948cbfecffed5ed53e3475fa58e503712c44cf1
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-12-23 13:45:11 -0300

    Att(Gráfico Pizza)

--- Scripts/new_chart.py
+++ Scripts/new_chart.py
@@ -1,4 +1,21 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
 
-# dados = pd.read_csv('dados.csv')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+arquivo = pd.read_excel('Dados_Cobaia.xlsx')
+
+
+
+def graficoPizza():
+    Entregue = arquivo.loc[arquivo['Status']=='Entregue']
+    Pendente = arquivo.loc[arquivo['Status']=='Pendente']
+    plt.title('Status dos pedidos')
+    plt.pie([len(Entregue),len(Pendente)], labels=['Entregue','Pendente'], autopct='%1.1f%%')
+    plt.axis('equal')
+    plt.show()
+
+graficoPizza()
+
