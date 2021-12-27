252e6d37b16dc279bce3423adfe8c147bf741ecb
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-12-27 20:59:40 -0300

    att

--- Scripts/reports.py
+++ Scripts/reports.py
@@ -2,91 +2,104 @@ from reportlab.pdfgen import canvas
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import PySimpleGUI as sg
 
 arquivo = pd.read_excel('dados.xlsx')
 
 def historico_todos_pedidos():
-    file_name = input('Digite o nome do arquivo: ')
+
+    file_name = f'Histórico de todos os pedidos({datetime.datetime.now().strftime("%d-%m-%Y")})'
     pdf = canvas.Canvas(file_name + '.pdf')
-    pdf.drawString(100,750,'Histórico de pedidos')
+    pdf.setPageSize((750,1000))
 
+    pdf.drawString(100,950,'Histórico de todos os pedidos')
     pdf.setFont('Helvetica', 12)
-    pdf.drawString(10,700,'ID')
-    pdf.drawString(50,700,'Nome cliente')
-    pdf.drawString(150,700,'Data de entrega')
-    pdf.drawString(250,700,'B. Aniversário')
-    pdf.drawString(350,700,'B. Casamento')
-    pdf.drawString(440,700,'S. Mini')
-    pdf.drawString(500,700,'S. Normal')
-
+    pdf.drawString(10,900,'ID')
+    pdf.drawString(50,900,'Nome cliente')
+    pdf.drawString(150,900,'Data de entrega')
+    pdf.drawString(250,900,'B. Aniversário')
+    pdf.drawString(340,900,'B. Casamento')
+    pdf.drawString(430,900,'S. Mini')
+    pdf.drawString(485,900,'S. Normal')
+    pdf.drawString(550,900,'Valor final')
+    pdf.drawString(630,900,'Status')
 
     pdf.setFont('Helvetica', 10)
     for i in range(len(arquivo)):
-        pdf.drawString(10,650-i*20,str(arquivo['ID'][i]))
-        pdf.drawString(50,650-i*20,str(arquivo['Nome cliente'][i]))
-        pdf.drawString(150,650-i*20,str(arquivo['Data de entrega'][i]))
-        pdf.drawString(270,650-i*20,str(arquivo['Bolo de aniversário'][i]))
-        pdf.drawString(370,650-i*20,str(arquivo['Bolo de casamento'][i]))
-        pdf.drawString(450,650-i*20,str(arquivo['Salgado mini'][i]))
-        pdf.drawString(510,650-i*20,str(arquivo['Salgado normal'][i]))
-
+        pdf.drawString(10,850-i*20,str(arquivo['ID'][i]))
+        pdf.drawString(50,850-i*20,str(arquivo['Nome cliente'][i]))
+        pdf.drawString(150,850-i*20,str(arquivo['Data de entrega'][i]))
+        pdf.drawString(290,850-i*20,str(arquivo['Bolo de aniversário'][i]))
+        pdf.drawString(360,850-i*20,str(arquivo['Bolo de casamento'][i]))
+        pdf.drawString(460,850-i*20,str(arquivo['Salgado mini'][i]))
+        pdf.drawString(495,850-i*20,str(arquivo['Salgado normal'][i]))
+        pdf.drawString(560,850-i*20,str(arquivo['Valor final'][i]))
+        pdf.drawString(640,850-i*20,str(arquivo['Status'][i]))
+    
     pdf.save()
 
 
 def historico_pedidos_concluido():
-    file_name = input('Digite o nome do arquivo: ')
+    file_name = f'Pedidos Concluídos({datetime.datetime.now().strftime("%d-%m-%Y")})'
     pdf = canvas.Canvas(file_name + '.pdf')
-    pdf.drawString(100,750,'Histórico de pedidos concluídos')
+    pdf.setPageSize((750,1000))
+
+    pdf.drawString(100,950,'Histórico de pedidos concluídos')
     pdf.setFont('Helvetica', 12)
-    pdf.drawString(10,700,'ID')
-    pdf.drawString(50,700,'Nome cliente')
-    pdf.drawString(150,700,'Data de entrega')
-    pdf.drawString(250,700,'B. Aniversário')
-    pdf.drawString(340,700,'B. Casamento')
-    pdf.drawString(430,700,'S. Mini')
-    pdf.drawString(475,700,'S. Normal')
-    pdf.drawString(550,700,'Preço')
+    pdf.drawString(10,900,'ID')
+    pdf.drawString(50,900,'Nome cliente')
+    pdf.drawString(150,900,'Data de entrega')
+    pdf.drawString(250,900,'B. Aniversário')
+    pdf.drawString(340,900,'B. Casamento')
+    pdf.drawString(430,900,'S. Mini')
+    pdf.drawString(485,900,'S. Normal')
+    pdf.drawString(560,900,'Valor final')
+    pdf.drawString(630,900,'Status')
 
 
     pdf.setFont('Helvetica', 10)
     for i in range(len(arquivo)):
         if arquivo['Status'][i] == 'Concluído':
-            pdf.drawString(10,650-i*20,str(arquivo['ID'][i]))
-            pdf.drawString(50,650-i*20,str(arquivo['Nome cliente'][i]))
-            pdf.drawString(150,650-i*20,str(arquivo['Data de entrega'][i]))
-            pdf.drawString(270,650-i*20,str(arquivo['Bolo de aniversário'][i]))
-            pdf.drawString(360,650-i*20,str(arquivo['Bolo de casamento'][i]))
-            pdf.drawString(440,650-i*20,str(arquivo['Salgado mini'][i]))
-            pdf.drawString(485,650-i*20,str(arquivo['Salgado normal'][i]))
-            pdf.drawString(555,650-i*20,str(arquivo['Valor final'][i]))
+            pdf.drawString(10,850-i*20,str(arquivo['ID'][i]))
+            pdf.drawString(50,850-i*20,str(arquivo['Nome cliente'][i]))
+            pdf.drawString(150,850-i*20,str(arquivo['Data de entrega'][i]))
+            pdf.drawString(270,850-i*20,str(arquivo['Bolo de aniversário'][i]))
+            pdf.drawString(370,850-i*20,str(arquivo['Bolo de casamento'][i]))
+            pdf.drawString(440,850-i*20,str(arquivo['Salgado mini'][i]))
+            pdf.drawString(495,850-i*20,str(arquivo['Salgado normal'][i]))
+            pdf.drawString(570,850-i*20,str(arquivo['Valor final'][i]))
+            pdf.drawString(630,850-i*20,str(arquivo['Status'][i]))
 
     pdf.save()
 
 def historico_pedidos_naoentregues():
-    file_name = input('Digite o nome do arquivo: ')
+    file_name = f'Pedidos não entregues({datetime.datetime.now().strftime("%d-%m-%Y")})'
     pdf = canvas.Canvas(file_name + '.pdf')
-    pdf.drawString(100,750,'Pedidos que não foram entregues')
-    pdf.setFont('Helvetica', 12)
+    pdf.setPageSize((750,1000))
 
-    pdf.drawString(10,700,'ID')
-    pdf.drawString(50,700,'Nome cliente')
-    pdf.drawString(150,700,'Data de entrega')
-    pdf.drawString(250,700,'B. Aniversário')
-    pdf.drawString(340,700,'B. Casamento')
-    pdf.drawString(430,700,'S. Mini')
-    pdf.drawString(475,700,'S. Normal')
+    pdf.drawString(100,950,'Histórico de pedidos não entregues')
+    pdf.setFont('Helvetica', 12)
+    pdf.drawString(10,900,'ID')
+    pdf.drawString(50,900,'Nome cliente')
+    pdf.drawString(150,900,'Data de entrega')
+    pdf.drawString(250,900,'B. Aniversário')
+    pdf.drawString(340,900,'B. Casamento')
+    pdf.drawString(430,900,'S. Mini')
+    pdf.drawString(485, 900,'S. Normal')
+    pdf.drawString(560,900,'Status')
 
     pdf.setFont('Helvetica', 10)
     for i in range(len(arquivo)):
         if arquivo['Status'][i] == 'Pendente':
-            pdf.drawString(10,650-i*20,str(arquivo['ID'][i]))
-            pdf.drawString(50,650-i*20,str(arquivo['Nome cliente'][i]))
-            pdf.drawString(150,650-i*20,str(arquivo['Data de entrega'][i]))
-            pdf.drawString(270,650-i*20,str(arquivo['Bolo de aniversário'][i]))
-            pdf.drawString(360,650-i*20,str(arquivo['Bolo de casamento'][i]))
-            pdf.drawString(440,650-i*20,str(arquivo['Salgado mini'][i]))
-            pdf.drawString(485,650-i*20,str(arquivo['Salgado normal'][i]))
-            
+            pdf.drawString(10,850-i*20,str(arquivo['ID'][i]))
+            pdf.drawString(50,850-i*20,str(arquivo['Nome cliente'][i]))
+            pdf.drawString(150,850-i*20,str(arquivo['Data de entrega'][i]))
+            pdf.drawString(270,850-i*20,str(arquivo['Bolo de aniversário'][i]))
+            pdf.drawString(370,850-i*20,str(arquivo['Bolo de casamento'][i]))
+            pdf.drawString(440,850-i*20,str(arquivo['Salgado mini'][i]))
+            pdf.drawString(495,850-i*20,str(arquivo['Salgado normal'][i]))
+            pdf.drawString(570,850-i*20,str(arquivo['Status'][i]))
+        
     pdf.save()
 
 def pedidos_pendentes():
@@ -95,31 +108,36 @@ def pedidos_pendentes():
     datapedido = arquivo['Data de entrega']
     datapedido = pd.to_datetime(datapedido)
 
-    file_name = input('Digite o nome do arquivo: ')
+    file_name = f'Pedidos Pendentes({datetime.datetime.now().strftime("%d-%m-%Y")})'
     pdf = canvas.Canvas(file_name + '.pdf')
+    pdf.setPageSize((750,1000))
 
-    pdf.drawString(100,750,'Pedidos pendentes')
+    pdf.drawString(100,950,'Pedidos pendentes')
     pdf.setFont('Helvetica', 12)
-    pdf.drawString(10,700,'ID')
-    pdf.drawString(50,700,'Nome cliente')
-    pdf.drawString(150,700,'Data de entrega')
-    pdf.drawString(250,700,'B. Aniversário')
-    pdf.drawString(340,700,'B. Casamento')
-    pdf.drawString(430,700,'S. Mini')
-    pdf.drawString(475,700,'S. Normal')
+    pdf.drawString(10,900,'ID')
+    pdf.drawString(50,900,'Nome cliente')
+    pdf.drawString(150,900,'Data de entrega')
+    pdf.drawString(250,900,'B. Aniversário')
+    pdf.drawString(340,900,'B. Casamento')
+    pdf.drawString(430,900,'S. Mini')
+    pdf.drawString(485,900,'S. Normal')
+    pdf.drawString(560,900,'Status')
 
     pdf.setFont('Helvetica', 10)
     for i in range(len(arquivo)):
         if arquivo['Status'][i] == 'Pendente':
-            if data.date() > datapedido[i]:
-                pdf.drawString(10,650-i*20,str(arquivo['ID'][i]))
-                pdf.drawString(50,650-i*20,str(arquivo['Nome cliente'][i]))
-                pdf.drawString(150,650-i*20,str(arquivo['Data de entrega'][i]))
-                pdf.drawString(270,650-i*20,str(arquivo['Bolo de aniversário'][i]))
-                pdf.drawString(360,650-i*20,str(arquivo['Bolo de casamento'][i]))
-                pdf.drawString(440,650-i*20,str(arquivo['Salgado mini'][i]))
-                pdf.drawString(485,650-i*20,str(arquivo['Salgado normal'][i]))
+            if data > datapedido[i]:
+                pdf.drawString(10,850-i*20,str(arquivo['ID'][i]))
+                pdf.drawString(50,850-i*20,str(arquivo['Nome cliente'][i]))
+                pdf.drawString(150,850-i*20,str(arquivo['Data de entrega'][i]))
+                pdf.drawString(270,850-i*20,str(arquivo['Bolo de aniversário'][i]))
+                pdf.drawString(370,850-i*20,str(arquivo['Bolo de casamento'][i]))
+                pdf.drawString(440,850-i*20,str(arquivo['Salgado mini'][i]))
+                pdf.drawString(495,850-i*20,str(arquivo['Salgado normal'][i]))
+                pdf.drawString(560,850-i*20,str(arquivo['Status'][i]))
+
             
     pdf.save()
 
-historico_todos_pedidos()
+
+historico_pedidos_naoentregues()
