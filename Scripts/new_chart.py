1a56de5077aa3856ca3060bf777f4fba26d8d2ae
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-12-27 14:44:50 -0300

    Deus é bom, rodou

--- Scripts/new_chart.py
+++ Scripts/new_chart.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-import PySimpleGUI as sg
-from reportlab.pdfgen import canvas
-
 
 arquivo = pd.read_excel('dados.xlsx')
 
@@ -78,35 +75,3 @@ class NewChart:
         plt.pie([salgadosM, salgadoN], labels=['Salgado mini','Salgado normal'], autopct='%1.1f%%')
         plt.axis('equal')
         plt.show()
-
-
-
-def historico_pedidos():
-    file_name = input('Digite o nome do arquivo: ')
-    pdf = canvas.Canvas(file_name + '.pdf')
-    pdf.drawString(100,750,'Histórico de pedidos')
-
-    pdf.setFont('Helvetica', 12)
-    pdf.drawString(10,700,'ID')
-    pdf.drawString(50,700,'Nome cliente')
-    pdf.drawString(150,700,'Data de entrega')
-    pdf.drawString(250,700,'B. Aniversário')
-    pdf.drawString(350,700,'B. Casamento')
-    pdf.drawString(440,700,'S. Mini')
-    pdf.drawString(500,700,'S. Normal')
-
-
-    pdf.setFont('Helvetica', 10)
-    for i in range(len(arquivo)):
-        pdf.drawString(10,650-i*20,str(arquivo['ID'][i]))
-        pdf.drawString(50,650-i*20,str(arquivo['Nome cliente'][i]))
-        pdf.drawString(150,650-i*20,str(arquivo['Data de entrega'][i]))
-        pdf.drawString(270,650-i*20,str(arquivo['Bolo de aniversário'][i]))
-        pdf.drawString(370,650-i*20,str(arquivo['Bolo de casamento'][i]))
-        pdf.drawString(450,650-i*20,str(arquivo['Salgado mini'][i]))
-        pdf.drawString(510,650-i*20,str(arquivo['Salgado normal'][i]))
-
-    pdf.save()
-
-
-historico_pedidos()
