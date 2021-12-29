07161bf810cd3815c704c5ace0f661fe2ea5720b
Jean Carlos <jeancarllos99@outlook.com>
2021-12-29 14:29:30 -0300

    Otimizações feitas no SETUP

--- setup.py
+++ setup.py
@@ -28,12 +28,15 @@ while True:
     if janela == menu:
         if evento == "-NOVA_ENCOMENDA-":
             nova_encomenda = NovaEncomenda.nova_encomenda()
+            continue
 
         elif evento == "-LISTAR_ENCOMENDAS-":
             menu_encomenda = ListarEncomendas.listar_encomendas("Pendente")
+            continue
 
         elif evento == "-DAR_BAIXA_ENCOMENDA-":
             dar_baixa_encomenda = BaixaEncomenda.baixa_encomenda()
+            continue
 
         elif evento == "-SAIR-":
             break
@@ -44,6 +47,7 @@ while True:
     ##########################################################################
     if janela == nova_encomenda and evento == sg.WIN_CLOSED or janela == nova_encomenda and evento == "-VOLTAR-":
         nova_encomenda.hide()
+        continue
 
 
     if janela == nova_encomenda and evento == "-CONFIRMAR-":
@@ -60,6 +64,7 @@ while True:
 
         sg.popup("Encomenda cadastrada com sucesso!")
         nova_encomenda.hide()
+        continue
 
         
     ##########################################################################
@@ -70,6 +75,7 @@ while True:
 
     if janela == menu_encomenda and evento == sg.WIN_CLOSED or janela == menu_encomenda and evento == "-VOLTAR-":
         menu_encomenda.hide()
+        continue
 
     if janela == menu_encomenda and evento == "-FILTRAR-":
         status_pendente = valor["-STATUS_PENDENTE-"] #true or false
@@ -83,6 +89,7 @@ while True:
             menu_encomenda.close()
             menu_encomenda = ListarEncomendas.listar_encomendas("Pendente")
             menu_encomenda["-STATUS_PENDENTE-"].update(True)
+        continue
 
     ##########################-MAIS_INFORMACOES-###############################
 
@@ -106,10 +113,12 @@ while True:
                 "Pendente", lista_clientes[index_da_lista], index_da_lista
                 )
             menu_encomenda.hide()
+        continue
 
     if janela == mais_informacoes and evento == sg.WIN_CLOSED or janela == mais_informacoes and evento == "-VOLTAR-":
         mais_informacoes.hide()
         menu_encomenda.un_hide()
+        continue
 
 
     ##########################################################################
@@ -120,6 +129,7 @@ while True:
 
     if janela == dar_baixa_encomenda and evento == sg.WIN_CLOSED or janela == dar_baixa_encomenda and evento == "-VOLTAR-":
         dar_baixa_encomenda.hide()
+        continue
 
     if janela == dar_baixa_encomenda and evento == "-FINALIZAR_ENCOMENDA-":
         index_encomenda = valor["-TABLE_LISTAR_ENCOMENDA-"]
@@ -134,13 +144,15 @@ while True:
 
         dar_baixa_encomenda["-VALOR_FINAL-"].update("R$" + str(preco_final))
         dar_baixa_encomenda["-FINALIZAR_ENCOMENDA-"].update(disabled=True)
+        continue
 
     if janela == dar_baixa_encomenda and evento == "-ATUALIZAR_LISTA-":
         dar_baixa_encomenda["-TABLE_LISTAR_ENCOMENDA-"].update(
             DataList("Pendente").get_dados_pedido_resumido()
             )
-            
+
         dar_baixa_encomenda["-FINALIZAR_ENCOMENDA-"].update(disabled=False)
         dar_baixa_encomenda["-VALOR_FINAL-"].update("R$0,00")
         dar_baixa_encomenda["-BOLO_ANIVERSARIO-"].update(0)
         dar_baixa_encomenda["-BOLO_CASAMENTO-"].update(0)
+        continue
