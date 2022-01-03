698481216accdf66fe74c4cb0a2ee93d5f43c24e
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2022-01-03 20:34:00 -0300

    att

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+!/bin/sh
+sudo pip3 install --upgrade openpyxl
+sudo pip3 install --upgrade pandas
+sudo pip3 install --upgrade numpy
+sudo pip3 install --upgrade matplotlib
+sudo pip3 install --upgrade datetime
+sudo pip3 install --upgrade reportlab
+sudo apt-get install python-tk python3-tk -y
+
