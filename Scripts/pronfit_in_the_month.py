4d574598b7e89e8d234c6786c5aeafc5d7e6df2a
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2022-01-04 09:50:51 -0300

    att

--- Scripts/pronfit_in_the_month.py
+++ Scripts/pronfit_in_the_month.py
@@ -6,7 +6,7 @@ class Gasto:
     def descobrirMes():
         mes = datetime.datetime.now().month
         return mes
-    def descobrirGamhoMes():
+    def descobrirGanhoMes():
         datas = Xlsx_to_list("C").toListStr()
         status = Xlsx_to_list("K").toListStr()
         valor = Xlsx_to_list("I").toListStr()
@@ -69,22 +69,25 @@ class Gasto:
 
 class Lucromensal:
     def lucro():
+        sg.theme('DefaultNoMoreNagging')
         layout = [
-            [
-                [sg.Text('Controle de Gastos')],
-                [sg.Text('', size=(100, 1), font=("Helvetica", 25), key='-OUTPUT-')],
-                [sg.Text('Quanto você gastou nesse mês com funcionários?',size=(100,1), font=("Helvetica", 15))],
-                [sg.InputText(size=(100, 1), key='-INPUT_FUNCIONARIOS-', default_text='0')],
-                [sg.Text('Quanto você gastou nesse mês com mercadorias?',size=(100,1), font=("Helvetica", 15))],
-                [sg.InputText(size=(100, 1), key='-INPUT_MERCADORIAS-', default_text='0')],
-                [sg.Text('Quanto você gastou nesse mês com impostos?',size=(100,1), font=("Helvetica", 15))],
-                [sg.InputText(size=(100, 1), key='-INPUT_IMPOSTOS-', default_text='0')],
-                [sg.Text('Quanto você gastou nesse mês com outros custos?',size=(100,1), font=("Helvetica", 15))],
-                [sg.InputText(size=(100, 1), key='-INPUT_OUTROS-', default_text='0')],
-                [sg.Button('Enviar', key='-ENVIAR-', size=(30,1)), sg.Button('Exit', key='-EXIT-', size=(30,1))],
-            ]
+            [sg.Frame('Informe os gastos no atual mês',
+                [
+                    [sg.Text('Controle de Gastos')],
+                    [sg.Text('', size=(100, 1), font=("Helvetica", 25), key='-OUTPUT-')],
+                    [sg.Text('Quanto você gastou nesse mês com funcionários?',size=(100,1), font=("Helvetica", 15))],
+                    [sg.InputText(size=(100, 1), key='-INPUT_FUNCIONARIOS-', default_text='0')],
+                    [sg.Text('Quanto você gastou nesse mês com mercadorias?',size=(100,1), font=("Helvetica", 15))],
+                    [sg.InputText(size=(100, 1), key='-INPUT_MERCADORIAS-', default_text='0')],
+                    [sg.Text('Quanto você gastou nesse mês com impostos?',size=(100,1), font=("Helvetica", 15))],
+                    [sg.InputText(size=(100, 1), key='-INPUT_IMPOSTOS-', default_text='0')],
+                    [sg.Text('Quanto você gastou nesse mês com outros custos?',size=(100,1), font=("Helvetica", 15))],
+                    [sg.InputText(size=(100, 1), key='-INPUT_OUTROS-', default_text='0')],
+                    [sg.Button('Enviar', key='-ENVIAR-', size=(30,1), button_color=("White", "#FF8C01")), sg.Button('Sair', key='-EXIT-', size=(30,1), button_color=("White", "#FF8C01"))],
+                ],
+            )],
         ]
-        window = sg.Window('Controle de Gastos', layout, size=(500, 500))
+        window = sg.Window('Controle de Gastos', layout, size=(500, 400))
         while True:
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == '-EXIT-':
@@ -95,9 +98,9 @@ class Lucromensal:
                     mercadorias = float(values['-INPUT_MERCADORIAS-'])
                     impostos = float(values['-INPUT_IMPOSTOS-'])
                     outros = float(values['-INPUT_OUTROS-'])
-                    total = funcionarios + mercadorias + impostos + outros - Gasto.descobrirGamhoMes()
+                    total = funcionarios - mercadorias - impostos - outros + Gasto.descobrirGanhoMes()
                     window['-OUTPUT-'].update(total)
-                    window['-OUTPUT-'].update(f'O lucro total: {total} reais')
+                    window['-OUTPUT-'].update(f'O lucro mensal será {total} reais')
                 except ValueError:
                     window['-OUTPUT-'].update('Por favor, digite apenas números.')
             if event == '-EXIT-':
