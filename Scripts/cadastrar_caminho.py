218a7705a3fbd5e3bc4ce17091ffb5ed8c57dbab
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-01-27 10:00:11 -0300

    att

--- Scripts/cadastrar_caminho.py
+++ Scripts/cadastrar_caminho.py
@@ -1,4 +1,5 @@
-from tkinter import filedialog
+from turtle import color
+import PySimpleGUI as sg
 import os
 from click import option
 
@@ -7,7 +8,8 @@ keys_caminho = 'caminhos.csv'
 class caminho:
     def criar():
         if os.stat(keys_caminho).st_size == 0:
-            pasta = filedialog.askdirectory()
+            # Criar um popup branco pra escolher a pasta
+            pasta = sg.PopupGetFolder('Escolha a pasta de destino')
             with open(keys_caminho, 'w') as arquivo:
                 arquivo.write(pasta + '/backup_dados.db\n')
 
