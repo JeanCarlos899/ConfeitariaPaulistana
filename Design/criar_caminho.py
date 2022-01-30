be8a45f3477dba2d92bdcd2f39dc44056f1f555f
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-01-30 12:08:38 -0300

    att

--- Design/criar_caminho.py
+++ Design/criar_caminho.py
@@ -7,16 +7,16 @@ class FrtCam:
         layout = [
             [sg.Frame('informe o caminho onde ficará o backup do banco de dados',
                 [                    
-                    [sg.Button('Procurar', key='-PROCURAR-', size=(100,1), button_color=("White", "#FF8C01"))],
+                    [sg.Button(image_filename="Design/Images/escolher.png", button_color=("#E8E5EA", "#E8E5EA"), key="-PROCURAR-",image_size=(500, 100), pad=(5, 5))],
                     
-                ], size=(500, 100)
+                ], size=(500, 150)
             )],
 
             [sg.Frame('Já tem um caminho cadastrado? Informe um novo caminho',
                 [
-                    [sg.Button('Editar Caminho', key='-EDITAR-', size=(100,1), button_color=("White", "#FF8C01"))],
+                    [sg.Button(image_filename="Design/Images/editar.png", button_color=("#E8E5EA", "#E8E5EA"), key="-EDITAR-",image_size=(500, 100), pad=(5, 5))],
     
-              ], size=(500, 100)
+              ], size=(500, 150)
             )],
             [sg.Button('Voltar', key='-VOLTAR-', size=(100,1), button_color=("White", "#FF8C01"))],
         ]
